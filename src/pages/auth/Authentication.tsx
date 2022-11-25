631416e9879b7c9f495a4473c25719d4bf9ee4ac
Karsten Krause <karsten.krause1988@gmail.com>
2022-11-25 15:55:19 +0100

    Added final loginpage

--- src/pages/auth/Authentication.tsx
+++ src/pages/auth/Authentication.tsx
@@ -3,50 +3,47 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import LoginPage from "./login/Login";
-import RegistrationPage from "./registration/Registration";
 import "./style/authentication.css";
 
-interface Props {}
-interface State {}
+interface IAuthPageProps {}
+interface IAuthPageState {}
 
-class AuthenticationPage extends React.Component<Props, State> {
-  render() {
-    return (
-      <main>
-        <div className="container">
-          <aside>
-            <div className="content-container">
-              <h1>Bandmate</h1>
-              <article>
-                <p>Vernetze dich mit Bands und Musikern aus deiner Umgebung.</p>
-              </article>
-            </div>
-          </aside>
+const AuthenticationPage: React.FunctionComponent<IAuthPageProps> = (props) => {
+  return (
+    <main>
+      <div className="container">
+        <aside>
+          <div className="content-container">
+            <h1>Bandmate</h1>
+            <article>
+              <p>Vernetze dich mit Bands und Musikern aus deiner Umgebung.</p>
+            </article>
+          </div>
+        </aside>
 
-          <div className="forms-container">
-            <div className="login-container">
-              <LoginPage />
-              <div className="reg-container">
-                <div className="divider">ODER</div>
-                <p id="reg-p">Registrieren mit Email, Apple oder Google</p>
-                <div className="reg-buttons-container">
-                  <button className="reg-button">
-                    <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
-                  </button>
-                  <button className="reg-button">
-                    <FontAwesomeIcon icon={faApple}></FontAwesomeIcon>
-                  </button>
-                  <button className="reg-button">
-                    <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
-                  </button>
-                </div>
+        <div className="forms-container">
+          <div className="login-container">
+            <LoginPage />
+            <div className="reg-container">
+              <div className="divider">ODER</div>
+              <p id="reg-p">Registrieren mit Email, Apple oder Google</p>
+              <div className="reg-buttons-container">
+                <button className="reg-button">
+                  <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+                </button>
+                <button className="reg-button">
+                  <FontAwesomeIcon icon={faApple}></FontAwesomeIcon>
+                </button>
+                <button className="reg-button">
+                  <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
+                </button>
               </div>
             </div>
           </div>
         </div>
-      </main>
-    );
-  }
-}
+      </div>
+    </main>
+  );
+};
 
 export default AuthenticationPage;
--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -1,101 +1,92 @@
-import React, { ChangeEvent, FormEvent } from "react";
+import React, { useState } from "react";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export interface Props {}
+const LoginPage: React.FunctionComponent = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-export interface State {
-  email: string;
-  password: string;
-  //redirect: string | null;
-  //loading: boolean;
-  //message: string;
-}
+  const handleChangeEmailInput = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEmail(event.target.value);
+  };
 
-export class LoginPage extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      //redirect: null,
-      //loading: false,
-      //message: ''
-    };
-  }
+  const handleChangePasswordInput = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPassword(event.target.value);
+  };
 
-  handleChangeUsernameInput(
-    event: ChangeEvent<
-      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-    >
-  ) {
-    this.setState({
-      email: event.target.value,
-    });
-  }
-
-  handleChangePasswordInput(
-    event: ChangeEvent<
-      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-    >
-  ) {
-    this.setState({
-      password: event.target.value,
-    });
-  }
-
-  handleSubmit(event: FormEvent) {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // TODO: implementation of the login logic
     // debug
-    console.log("Username: " + this.state.email);
-    console.log("Passwort: " + this.state.password);
-    this.setState({
-      email: "",
-      password: "",
-    });
-  }
-
-  render() {
-    return (
-      <form
-        className="login-form"
-        onSubmit={(event) => this.handleSubmit(event)}
-      >
-        <h2 className="title">Anmelden</h2>
-        <div className="input-field">
-          <i className="form-icons">
-            <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
-          </i>
-
-          <input
-            placeholder="Email"
-            value={this.state.email}
-            onChange={(event) => this.handleChangeUsernameInput(event)}
-          ></input>
-        </div>
-        <div className="input-field">
-          <i className="form-icons">
-            <FontAwesomeIcon icon={faLock}></FontAwesomeIcon>
-          </i>
-
-          <input
-            placeholder="Passwort"
-            type="password"
-            value={this.state.password}
-            onChange={(event) => this.handleChangePasswordInput(event)}
-          ></input>
-        </div>
-        <button className="form-button" type="submit">
-          Login
-        </button>
-        <p id="error-message">Das Password oder die Email ist falsch.</p>
-
-        
-      </form>
+    /*
+    console.log("Username: " + email);
+    console.log("Passwort: " + password);
+    console.log(
+      "Beide Inputs erfolgreich validiert?: " + validateForm(email, password)
     );
-  }
-}
+    */
+
+    validateForm(email, password)
+      ? setErrorMessage("")
+      : setErrorMessage("Email oder Passwort ist falsch");
+
+    resetStates();
+  };
+
+  const resetStates = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const validateForm = (emailInput: string, passwordInput: string): boolean => {
+    const emailValide: RegExp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+
+    const passwordValide: RegExp = /^[a-zA-Z0-9]{6,}$/g;
+
+    return emailValide.test(emailInput) && passwordValide.test(passwordInput);
+  };
+
+  return (
+    <form className="login-form" onSubmit={onSubmit}>
+      <h2 className="title">Anmelden</h2>
+      <div className="input-field">
+        <i className="form-icons">
+          <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+        </i>
+
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={handleChangeEmailInput}
+          required
+        ></input>
+      </div>
+      <div className="input-field">
+        <i className="form-icons">
+          <FontAwesomeIcon icon={faLock}></FontAwesomeIcon>
+        </i>
+
+        <input
+          placeholder="Passwort"
+          type="password"
+          value={password}
+          onChange={handleChangePasswordInput}
+          required
+        ></input>
+      </div>
+      <button className="form-button" type="submit">
+        Login
+      </button>
+      <p id="error-message">{errorMessage}</p>
+    </form>
+  );
+};
 
 export default LoginPage;
