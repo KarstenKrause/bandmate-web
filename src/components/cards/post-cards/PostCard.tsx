b687138a20f6355ab18fcdc9b6ad12c00e3d8b80
Karsten Krause <karsten.krause1988@gmail.com>
2023-08-25 23:46:20 +0200

    added posts testdata + styling

--- src/components/cards/post-cards/PostCard.tsx
+++ src/components/cards/post-cards/PostCard.tsx
@@ -3,13 +3,16 @@ import { FunctionComponent } from 'react';
 import styles from "./PostCard.module.scss"
 
 interface PostCardProps {
-  
+  description: string
 }
 
-const PostCard: FunctionComponent<PostCardProps> = () => {
-  return (  
-    <div className={styles["post-card"]}>
-      
+const PostCard: FunctionComponent<PostCardProps> = ({ description }) => {
+  return (
+    <div className={`${styles["post-card"]}`}>
+      <div className={`${styles["description-container"]}`}>
+        <p>{description}</p>
+      </div>
+
     </div>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,23 +2,41 @@ import React from 'react'
 import "./home.scss"
 import PostCard from '../../components/cards/post-cards/PostCard';
 
+
+const testPostCardsData = [
+  {
+    description: "kljsdlkjds"
+  },
+  {
+    description: "kljsdlkjds"
+  },
+  {
+    description: "kljsdlkjds"
+  },
+  {
+    description: "kljsdlkjds"
+  },
+]
 export interface IHomePageProps {
-    
+
 }
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
-    return (
-      <div className="home-container">
-        <h1>Beiträge</h1>
-        <div className='posts-container'>
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-        </div>
-
+  return (
+    <div className="home-container">
+      <h1>Neu auf Bandmate</h1>
+      <h1>Beiträge</h1>
+      <div className='posts-container'>
+        
+        {
+          testPostCardsData.map(post => (
+            <PostCard description={post.description}/>
+          ))
+        }
       </div>
-    );
+
+    </div>
+  );
 }
 
 export default HomePage;
